2a723efa5466cd8c4514873d9bd5b471fcc39309
Manuel Cartuche <macartuche@gmail.com>
2016-08-26 22:07:05 -0500

    agregando librerias para obtener modelos a partir de la bd

---
+++ app/Test.php
@@ -0,0 +1,16 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Test extends Model {
+
+    /**
+     * Generated
+     */
+
+    protected $table = 'test';
+    protected $fillable = ['id', 'description'];
+
+
+
+}
